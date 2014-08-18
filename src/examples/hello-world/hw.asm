1a96ff98fd29f29fa27138ffc42b2d632e4d2a80
Dmitry Khokhlov <khokhlov@targetix.net>
2014-08-19 00:02:22 +0400

    hello-world: добавить вызов CreateProcess и TerminateProcess

--- src/examples/hello-world/hw.asm
+++ src/examples/hello-world/hw.asm
@@ -3,11 +3,19 @@
                 .code
 
 _Start:
-                mov     ah, 09h
+                mov     ah, 0x09
                 mov     dx, offset _message
-                int     21h
-                retf
+                int     0x21
 
-_message        db      "Hello, World!", 0Dh, 0Ah, '$'
+                mov     di, 0
+                mov     ax, offset _executablePath
+                mov     dx, 21
+                int     0x90
+
+                mov     di, 1
+                int     0x90
+
+_message        db      "Hello, World!", 0x0Dh, 0x0A, '$'
+_executablePath db      "randstr", 0
 
                 end     _Start
